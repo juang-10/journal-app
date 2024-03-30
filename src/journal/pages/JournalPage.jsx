7b6226d96242149fd4e2fb04ead2a6e656b11463
david <judagosa8520@gmail.com>
2024-03-30 11:29:58 -0500

    refactor: add basic JournalPage with Typography component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,9 @@
+import { Typography } from "@mui/material"
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <>
+      <Typography component='h1'>JournalPage</Typography>
+    </>
   )
 }
