1c625d4780f2d3aabdab6bdf7056c0a8fd85954a
david <judagosa8520@gmail.com>
2024-03-30 14:20:50 -0500

    feat: Wrap JournalPage content with JournalLayout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography component='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, pariatur nostrum. A aliquam molestiae placeat ipsum, incidunt fugit alias reprehenderit. Dolorem, alias facere modi unde veritatis reprehenderit praesentium fuga minima!</Typography>
+    </JournalLayout>
   )
 }
