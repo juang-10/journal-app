d56edec6dea3fd7c6819b02ff84179bc3af31c3a
david <judagosa8520@gmail.com>
2024-03-31 16:45:33 -0500

    feat: Integrate NothingSelectedView into JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, pariatur nostrum. A aliquam molestiae placeat ipsum, incidunt fugit alias reprehenderit. Dolorem, alias facere modi unde veritatis reprehenderit praesentium fuga minima!</Typography>
+    
+      <NothingSelectedView />
+      {/* NoteView */}
     </JournalLayout>
   )
 }
