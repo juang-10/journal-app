3577e5ddbd24fb1a721416b9c26eac4d6888006c
david <judagosa8520@gmail.com>
2024-04-14 21:00:35 -0500

    feat: Integrate useForm hook and handle form submission in LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,18 +2,33 @@ import { Link as RouterLink } from 'react-router-dom'
 import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks'
 
 export const LoginPage = () => {
+
+  const { email, password, onInputChange } =  useForm({
+    email: 'juan@google.com',
+    password: '123456'
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log({email, password});
+  }
+
   return (
     <AuthLayout title="Login">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2}}>
             <TextField 
               label="Correo" 
               type="email" 
               placeholder="correo@correo.com"
-              fullWidth 
+              fullWidth
+              name='email'
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
           
@@ -22,13 +37,16 @@ export const LoginPage = () => {
               label="Contraseña" 
               type="password" 
               placeholder="Contraseña"
-              fullWidth 
+              fullWidth
+              name='password'
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1}}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button type='submit' variant="contained" fullWidth>
                 Login
               </Button>
             </Grid>
