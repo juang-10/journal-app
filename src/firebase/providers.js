403cf4e401845230128fc1e017ba0a7012976553
juan <judagosa8520@gmail.com>
2024-04-20 14:47:24 -0500

    Refactor: Add registerWithEmailPassword function to handle user registration with email and password

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { FirebaseAuth } from "./config";
 
 const googleProvider = new GoogleAuthProvider();
@@ -21,6 +21,26 @@ export const signInWithGoogle = async() => {
     return {
       ok: false,
       errorMessage
+    }
+  }
+}
+
+export const registerWithEmailPassword = async({email, password, displayName}) => {
+  console.log("🚀 ~ registerWithEmailPassword ~ email:", email)
+  try {
+    const resp = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
+    const { uid, photoURL } = resp.user;
+    console.log("🚀 ~ registerWithEmailPassword ~ resp:", resp)
+    //TODO: Actualizar el displayName en Firebase
+
+    return {
+      uid, photoURL, displayName
+    }
+    
+  } catch (error) {
+    return {
+      ok: false,
+      errorMessage: error.message
     }
   }
 }
