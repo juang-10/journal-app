e59e8a2fe14c9415ca7cef1f6129037703974f0e
juan <judagosa8520@gmail.com>
2024-04-20 14:48:07 -0500

    Refactor: Add startEmailPasswordRegister thunk to handle user registration with email and password

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { signInWithGoogle } from "../../firebase/providers";
+import { registerWithEmailPassword, signInWithGoogle } from "../../firebase/providers";
 import { checkingCredentials, login, logout } from "./";
 
 export const checkingAuthencation = (email, password) => {
@@ -13,5 +13,14 @@ export const startGoogleSingIn = () => {
     const result = await signInWithGoogle();
     if(!result.ok) return dispatch( logout( result.errorMessage ) );
     dispatch( login( result ) );
+  }
+}
+
+export const startEmailPasswordRegister = ({email, password, displayName}) => {
+  return async(dispatch) => {
+    dispatch(checkingCredentials());
+    const resp = await registerWithEmailPassword({email, password, displayName});
+    console.log("🚀 ~ returnasync ~ resp:", resp)
+    
   }
 }
