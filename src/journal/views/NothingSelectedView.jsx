4a8ea9f957c50254389768fb106cf2bcb43da538
juan <judagosa8520@gmail.com>
2024-04-22 21:51:09 -0500

    feat: Add animations to the application

--- src/journal/views/NothingSelectedView.jsx
+++ src/journal/views/NothingSelectedView.jsx
@@ -1,9 +1,10 @@
-import { StartOutlined } from "@mui/icons-material"
+import { StarOutline } from "@mui/icons-material"
 import { Grid, Typography } from "@mui/material"
 
 export const NothingSelectedView = () => {
   return (
-    <Grid 
+    <Grid
+      className="animate__animated animate__fadeIn animate__faster"
       container
       spacing={0}
       direction={'column'}
@@ -12,7 +13,7 @@ export const NothingSelectedView = () => {
       sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.main', borderRadius: 3}}
     >
       <Grid item xs={12}>
-        <StartOutlined sx={{fontSize: 100, color: 'white'}} />
+        <StarOutline sx={{fontSize: 100, color: 'white'}} />
       </Grid>
       <Grid item xs={12}>
         <Typography variant='h5' color='white'>Selecciona o crea una entrada</Typography>
