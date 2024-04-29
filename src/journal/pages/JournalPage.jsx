0f659d480180b6b02d2643320524d32b03b2a242
juan <judagosa8520@gmail.com>
2024-04-29 14:38:41 -0500

    feat: JournalPage component to dispatch startNewNote thunk on new note button click

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,15 +2,25 @@ import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 import { AddOutlined } from "@mui/icons-material"
+import { useDispatch } from "react-redux"
+import { startNewNote } from "../../store/journal/thunks"
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  }
+
   return (
     <JournalLayout>
     
       <NothingSelectedView />
       {/* <NoteView/> */}
 
-      <IconButton 
+      <IconButton
+        onClick={onClickNewNote}
         size="large"
         sx={{
           color: 'white',
