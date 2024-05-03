ec76653d643194816810ecb198faf814da596e68
juan <judagosa8520@gmail.com>
2024-05-02 19:16:45 -0500

    refactor: journal thunks to use Firebase Firestore for loading notes

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,7 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote } from "./journalSlice";
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from "./journalSlice";
+import { loadNotes } from "../../helpers";
 
 export const startNewNote = () => {
   return async(dispatch, getState) => {
@@ -23,5 +24,14 @@ export const startNewNote = () => {
     //!dispatch
     dispatch(addNewEmptyNote(newNote));
     dispatch(setActiveNote(newNote));
+  }
+}
+
+export const startLoadingNotes = () => {
+  return async(dispatch, getState) => {
+    const { uid } = getState().auth;
+    if(!uid) throw new Error('No uid found');
+    const notes = await loadNotes(uid);
+    dispatch(setNotes(notes));
   }
 }
