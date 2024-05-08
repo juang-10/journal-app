ee7de049d83d3270be6e91765b0cd5ab3ec07253
juan <judagosa8520@gmail.com>
2024-05-08 14:51:43 -0500

    Refactor: SideBar component to pass note props to SideBarItem

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -27,7 +27,7 @@ export const SideBar = ({drawerWidth = 240}) => {
         <List>
           {
             notes.map(note => (
-              <SideBarItem key={note.id} note={note}/>
+              <SideBarItem key={note.id} {...note}/>
             ))
           }
         </List>
