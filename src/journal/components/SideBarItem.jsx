3c1301a93a831ea26f431e865b1f02c524709cba
juan <judagosa8520@gmail.com>
2024-05-08 14:07:20 -0500

    feat: Add component that will display the notes in the SideBar

---
+++ src/journal/components/SideBarItem.jsx
@@ -0,0 +1,18 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+
+export const SideBarItem = ({note}) => {
+  return (
+    <ListItem key={note.id} disablePadding>
+      <ListItemButton>
+        <ListItemIcon>
+          <TurnedInNot />
+        </ListItemIcon>
+        <Grid container>
+          <ListItemText primary={note.title}/>
+          <ListItemText secondary={ note.body }/>
+        </Grid>
+      </ListItemButton>
+    </ListItem>
+  )
+}
