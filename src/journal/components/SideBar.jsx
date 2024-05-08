ca8ac9e4d0548211549d24e4668e1fc82a271812
juan <judagosa8520@gmail.com>
2024-05-08 14:07:28 -0500

    Refactor SideBar component to use SideBarItem for displaying notes

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,9 +1,10 @@
-import { TurnedInNot } from '@mui/icons-material'
-import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
+import { SideBarItem } from './SideBarItem';
 
 export const SideBar = ({drawerWidth = 240}) => {
   const { displayName } = useSelector( state => state.auth );
+  const { notes } = useSelector( state => state.journal );
 
   return (
     <Box
@@ -25,18 +26,8 @@ export const SideBar = ({drawerWidth = 240}) => {
 
         <List>
           {
-            ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'].map(text => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <TurnedInNot />
-                  </ListItemIcon>
-                  <Grid container>
-                    <ListItemText primary={text}/>
-                    <ListItemText secondary={'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'}/>
-                  </Grid>
-                </ListItemButton>
-              </ListItem>
+            notes.map(note => (
+              <SideBarItem key={note.id} note={note}/>
             ))
           }
         </List>
