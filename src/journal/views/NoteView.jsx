0af8afd8869e37357348a006743dd033da2f021d
juan <judagosa8520@gmail.com>
2024-05-09 08:17:04 -0500

    feat: Add Redux, hooks, dynamic date in NoteView

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,20 +1,48 @@
 import { SaveOutlined } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
+import { useForm } from "../../hooks/useForm"
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect, useMemo } from "react"
+import { setActiveNote, startSaveNote } from "../../store/journal"
 
 export const NoteView = () => {
+
+  const dispatch = useDispatch();
+
+  const { active:note } = useSelector(state => state.journal)
+
+  const { body, title, onInputChange, formState, date } = useForm(note);
+
+  const dateString = useMemo(() => {
+    const newDate = new Date(date);
+    return newDate.toUTCString();
+  }, [date]);
+
+  useEffect(() => {
+    dispatch(setActiveNote(formState))
+  }, [formState]);
+
+  const onSaveNote = () => {
+    dispatch( startSaveNote() );
+  }
+
   return (
     <Grid
       className="animate__animated animate__fadeIn animate__faster"
       container direction='row' 
-      justifyContent='space-between' 
+      justifyContent='space-between'
       alignItems='center' sx={{ mb: 1 }}
     >
       <Grid item>
-        <Typography fontSize={39} fontWeight='light'>28 agosto, 2023</Typography>
+        <Typography fontSize={39} fontWeight='light'>{ dateString }</Typography>
       </Grid>
       <Grid item>
-        <Button color="primary" sx={{ padding: 2 }}>
+        <Button 
+          color="primary" 
+          sx={{ padding: 2 }}
+          onClick={ onSaveNote }
+        >
           <SaveOutlined sx={{ fontSize: 30, mr: 1}} />
           Guardar
         </Button>
@@ -26,7 +54,10 @@ export const NoteView = () => {
           fullWidth 
           placeholder="Ingrese un título"
           label="Título"
-          sx={{ border: 'none', mb: 1 }} 
+          sx={{ border: 'none', mb: 1 }}
+          name="title"
+          value={title}
+          onChange={onInputChange}
         />
         <TextField 
           type="text"
@@ -35,6 +66,9 @@ export const NoteView = () => {
           multiline
           placeholder="¿Qué sucedió en el día de hoy?"
           minRows={5}
+          name="body"
+          value={body}
+          onChange={onInputChange}
         />
       </Grid>
 
