33aa7d79493b10a04bb0bc34d5c861d28097b42d
juan <judagosa8520@gmail.com>
2024-05-08 19:27:35 -0500

    refactor: LoginPage component to use pre-filled form data in LoginPage.jsx

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -7,15 +7,18 @@ import { useDispatch, useSelector } from 'react-redux'
 import { startGoogleSingIn, startLoginWithEmailPassword } from '../../store/auth'
 import { useMemo } from 'react'
 
+
+const formData = {
+  email: '',
+  password: ''
+}
+
 export const LoginPage = () => {
 
   const { status, errorMessage } = useSelector( state => state.auth );
 
   const dispatch = useDispatch();
-  const { email, password, onInputChange } =  useForm({
-    email: '',
-    password: ''
-  });
+  const { email, password, onInputChange } =  useForm(formData);
 
   const isAuthenticating = useMemo( () => status === 'checking', [status] );
 
