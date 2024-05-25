44d46c4730212eadc29bff023c06a76b74b29867
david <judagosa8520@gmail.com>
2024-05-24 19:03:32 -0500

    feat: Enable file uploading in NoteView component

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -4,7 +4,7 @@ import { ImageGallery } from "../components"
 import { useForm } from "../../hooks/useForm"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useMemo, useRef } from "react"
-import { setActiveNote, startSaveNote } from "../../store/journal"
+import { setActiveNote, startSaveNote, startUploadingFiles } from "../../store/journal"
 import Swal from "sweetalert2"
 import 'sweetalert2/dist/sweetalert2.css'
 
@@ -40,8 +40,7 @@ export const NoteView = () => {
 
   const onFileInputChange = ({target}) => {
     if( target.files.length === 0 ) return;
-    console.log('Subiendo archivos')
-    // dispatch( startUploadingFiles( target.files ));
+    dispatch( startUploadingFiles( target.files ));
   }
 
   return (
