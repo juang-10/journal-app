86f0d4f0e9b7d3160f4e83fde9f50659438d202f
david <judagosa8520@gmail.com>
2024-05-24 19:27:30 -0500

    feat: Update file uploading functionality in NoteView component

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,6 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setSaving, updateNote } from "./journalSlice";
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setPhotosToActiveNote, setSaving, updateNote } from "./journalSlice";
 import { fileUpload, loadNotes } from "../../helpers";
 
 export const startNewNote = () => {
@@ -52,6 +52,13 @@ export const startSaveNote = () => {
 export const startUploadingFiles = (files = []) => {
   return async(dispatch) => {
     dispatch(setSaving());
-    await fileUpload(files[0]);
+    // await fileUpload(files[0]);
+    const fileUploadPromises = [];
+    for (const file of files) {
+      fileUploadPromises.push(fileUpload(file));
+    }
+
+    const photosUrl = await Promise.all(fileUploadPromises);
+    dispatch(setPhotosToActiveNote(photosUrl));
   }
 }
