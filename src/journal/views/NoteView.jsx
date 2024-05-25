752fecf8b697da1c90bbd00ff82e871b2ba36d70
david <judagosa8520@gmail.com>
2024-05-24 20:48:00 -0500

    refactor: Add deleteNoteById action and thunk for deleting a note

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,10 +1,10 @@
-import { SaveOutlined, UploadOutlined } from "@mui/icons-material"
+import { DeleteOutline, SaveOutlined, UploadOutlined } from "@mui/icons-material"
 import { Button, Grid, IconButton, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
 import { useForm } from "../../hooks/useForm"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useMemo, useRef } from "react"
-import { setActiveNote, startSaveNote, startUploadingFiles } from "../../store/journal"
+import { setActiveNote, startDeletingNote, startSaveNote, startUploadingFiles } from "../../store/journal"
 import Swal from "sweetalert2"
 import 'sweetalert2/dist/sweetalert2.css'
 
@@ -41,6 +41,10 @@ export const NoteView = () => {
   const onFileInputChange = ({target}) => {
     if( target.files.length === 0 ) return;
     dispatch( startUploadingFiles( target.files ));
+  }
+
+  const onDelete = () => {
+    dispatch(startDeletingNote());
   }
 
   return (
@@ -101,6 +105,17 @@ export const NoteView = () => {
           value={body}
           onChange={onInputChange}
         />
+      </Grid>
+
+      <Grid container justifyContent='end'>
+        <Button
+          onClick={ onDelete }
+          sx={{ mt: 2 }}
+          color="error"
+        >
+          <DeleteOutline/>
+          Borrar
+        </Button>
       </Grid>
 
       <ImageGallery images={ note.imageUrls }/>
