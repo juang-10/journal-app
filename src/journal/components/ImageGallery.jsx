8c55919e49cdd95ebd42a9ef10f8149bf90615d0
david <judagosa8520@gmail.com>
2024-05-24 20:03:06 -0500

    feat: Remove console.log statement in ImageGallery component

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -2,7 +2,6 @@ import { ImageList, ImageListItem } from "@mui/material";
 
 
 export const ImageGallery = ({images}) => {
-  console.log("🚀 ~ ImageGallery ~ images:", images)
   return (
     <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={350}>
       {images.map((image) => (
