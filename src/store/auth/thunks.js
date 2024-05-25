bc9cddeba9e05fb0ff5c6f27a5b1f1208529322a
david <judagosa8520@gmail.com>
2024-05-24 20:20:47 -0500

    refactor: Clear notes and reset state on user logout

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,5 @@
 import { loginWithEmailPassword, logoutFirebase, registerUserWithEmailPassword, signInWithGoogle } from "../../firebase/providers";
+import { clearNotesLogout } from "../journal";
 import { checkingCredentials, login, logout } from "./";
 
 export const checkingAuthencation = (email, password) => {
@@ -37,6 +38,7 @@ export const startLoginWithEmailPassword = ({email, password}) => {
 export const startLogout = () => {
   return async(dispatch) => {
     await logoutFirebase();
+    dispatch(clearNotesLogout())
     dispatch( logout({}) );
   }
 }
